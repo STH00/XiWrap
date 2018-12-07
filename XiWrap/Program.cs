0d5cebfb68d9566e23698639b4de75b9b9b7de67
Jake Harrison <srharrs@admin.reynolds.edu>
2018-12-07 15:48:26 -0500

    Add project files.

---
+++ XiWrap/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace XiWrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Status file creator
+            ProcessAsUser.Launch(@"C:\Program Files (x86)\Xibo Player\XiWrap\XiWrap_Idle.exe");
+
+            while (true)
+            {
+                // Idle process watchdog
+                var idleWatch = Process.GetProcesses().Where(pr => pr.ProcessName.ToLower().Contains("xiwrap_idle"));
+                if (idleWatch.Count() > 1)
+                {
+                    foreach (var process in idleWatch)
+                    {
+                        process.Kill();
+                    }
+                }
+                else if (idleWatch.Count() < 1)
+                {
+                    ProcessAsUser.Launch(@"C:\Program Files (x86)\Xibo Player\XiWrap\XiWrap_Idle.exe");
+                }
+
+                // Variable containing process containing Xibo
+                var xiboProcess = Process.GetProcesses().Where(pr => pr.ProcessName.ToLower().Contains("xibo"));
+                // Status file path
+                var statusfile = @"C:\Program Files (x86)\Xibo Player\XiWrap\0.txt";
+
+                // If there is a Xibo process
+                if (xiboProcess.Count() > 0)
+                {
+                    // If not idle
+                    if (!File.Exists(statusfile))
+                    {
+                        // Kill every Xibo process
+                        foreach (var process in xiboProcess)
+                        {
+                            process.Kill();
+                        }
+                    }
+                }
+
+                // If there is not a Xibo process
+                if (xiboProcess.Count() < 1)
+                {
+                    // If idle
+                    if (File.Exists(statusfile))
+                    {
+                        ProcessAsUser.Launch(@"C:\Program Files (x86)\Xibo Player\Xibo.exe");
+                        //Process.Start(@"C:\Program Files (x86)\Xibo Player\Xibo.exe");
+                    }
+                }
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ XiWrap_Idle/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace XiWrap_Idle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var statusfile = @"C:\Program Files (x86)\Xibo Player\XiWrap\0.txt";
+            var configfile = @"C:\Program Files (x86)\Xibo Player\XiWrap\time.txt";
+            int timeout = 900000;
+            string configtime = "15";
+
+            //First run cleanup
+            if (File.Exists(statusfile))
+            {
+                File.Delete(statusfile);
+            }
+
+            //Check if config file exists
+            if (File.Exists(configfile))
+            {
+                //If blank delete and recreate.
+                if (File.ReadAllLines(configfile).Skip(1).Take(1).FirstOrDefault() == null)
+                {
+                    File.Delete(configfile);
+                    File.Create(configfile).Close();
+                    TextWriter tw = new StreamWriter(configfile);
+                    tw.WriteLine(@"//Enter the number of minutes before timeout on the second line of this file");
+                    tw.WriteLine(configtime);
+                    tw.Close();
+                }
+                //Else value exists.  Convert to miliseconds.
+                else
+                {
+                    configtime = File.ReadAllLines(configfile).Skip(1).Take(1).First();
+                    timeout = Convert.ToInt32(configtime) * 60000;
+                }
+            }
+            //If config file is missing, create default one.
+            else
+            {
+                File.Create(configfile).Close();
+                TextWriter tw = new StreamWriter(configfile);
+                tw.WriteLine(@"//Enter the number of minutes before timeout on the second line of this file");
+                tw.WriteLine(configtime);
+                tw.Close();
+            }
+
+            //Loop
+            while (true)
+            {
+                var idle = IdleTimeFinder.GetIdleTime();
+
+                //If high idle, create status file
+                if (idle > timeout)
+                {
+                    File.Create(statusfile).Close();
+                }
+
+                //If low idle, delete status file
+                if (idle < timeout)
+                {
+                    File.Delete(statusfile);
+                }
+                //Sleep
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
